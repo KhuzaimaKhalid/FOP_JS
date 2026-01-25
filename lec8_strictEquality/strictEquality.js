a6f13391af89026c9f2e4ccfc6b6001f17b0230e
Khuzaima Khalid <khuzaimakhalidkhan@gmail.com>
2026-01-25 22:48:24 +0500

    lec 8 added

---
+++ lec8_strictEquality/strictEquality.js
@@ -0,0 +1,25 @@
+const PI = 3.14
+
+if(PI == "3.14"){
+    console.log("This value is PI")
+} else{
+    console.log("This value is not PI")
+}
+
+if(PI === "3.14"){
+    console.log("This value is PI")
+} else{
+    console.log("This value is not PI")
+}
+
+if(PI != "3.14"){
+    console.log("This value is PI")
+} else{
+    console.log("This value is not PI")
+}
+
+if(PI !== "3.14"){
+    console.log("This value is PI")
+} else{
+    console.log("This value is not PI")
+}
