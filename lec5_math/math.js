8dfa51666c4e214fd9cab6280cbbfc52397a059d
Khuzaima Khalid <khuzaimakhalidkhan@gmail.com>
2026-01-25 15:46:37 +0500

    lec 5 added

---
+++ lec5_math/math.js
@@ -0,0 +1,24 @@
+let x = 3
+let y = 4
+let z = 5
+let a = 4.2
+let b = -4.7, c = 0
+
+console.log(Math.round(a))
+console.log(Math.ceil(a))
+console.log(Math.floor(a))
+console.log(Math.trunc(a))
+console.log(Math.sign(a))
+console.log(Math.sign(b))
+console.log(Math.sign(c))
+console.log(Math.pow(y,x))
+console.log(Math.sqrt(z))
+console.log(Math.abs(b))
+console.log(Math.max(x,y,z))
+console.log(Math.min(x,y,z))
+console.log(Math.random())
+console.log(Math.PI)
+console.log(Math.E)
+console.log(Math.sin(x))
+console.log(Math.cos(x))
+console.log(Math.tan(x))
