79a15a1ca6f380f756b47ce6d4c4f9a6aca92ff6
Khuzaima Khalid <khuzaimakhalidkhan@gmail.com>
2026-01-31 18:06:19 +0500

    lec 41 added

---
+++ fop_js/lec41_fetch/fetch.js
@@ -0,0 +1,38 @@
+fetch("https://pokeapi.co/api/v2/pokemon/Abomasnow")
+     .then(response => {
+        if(!response.ok){
+            throw new Error("Could not fetch data")
+        }
+        return response.json()
+     })
+     .then(data => {
+        console.log(data)
+     })
+     .catch(error => {
+        console.error(error)
+     })
+
+async function fetchData(){
+    try {
+        
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase() 
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+
+
+        if(!response.ok){
+            throw new Error("Could not fetch data")
+        }
+
+        const data = await response.json()
+
+        const pokemonSprite = data.sprites.front_default
+        const imgElement = document.getElementById("pokemonSprite")
+        imgElement.src = pokemonSprite
+        imgElement.style.display = "block"
+
+        console.log(data)
+    } catch (error) {
+        console.error(error)
+    }
+}
+fetchData()
